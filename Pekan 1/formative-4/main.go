e853d5a2d55097fc2ae539db7f3aaf8a24495dd5
Windy Dwi Febrianti <windydf25@gmail.com>
2025-01-10 10:27:58 +0700

    Update Tugas 4
    Tugas 4 - Array, Slice, Map & Looping

---
+++ Pekan 1/formative-4/main.go
@@ -0,0 +1,109 @@
+package main
+
+import "fmt"
+
+//soal 1
+func Soal1() {
+
+	var angka int
+	fmt.Scan(&angka)
+
+	if (angka%2 == 1) && (angka%3 != 0) {
+		fmt.Println("Santai")
+	} else if angka%2 == 0 {
+		fmt.Println("Berkualitas")
+	} else if (angka%3 == 0) && (angka%2 == 1) {
+		fmt.Println("I Love Coding")
+	}
+
+}
+
+//soal 2
+func Soal2() {
+
+	for i := 0; i <= 7; i++ {
+		for j := 0; j < i; j++ {
+			fmt.Print("#")
+		}
+		fmt.Println()
+	}
+}
+
+//soal 3
+func Soal3() {
+
+	var kalimat = [...]string{"aku", "dan", "saya", "sangat", "senang", "belajar", "golang"}
+	var kalimatBaru = kalimat [2:]
+
+	fmt.Println(kalimatBaru)
+}
+
+//soal 4 
+func Soal4() {
+
+	var sayuran = []string{ 
+		"Bayam",
+		"Buncis",
+		"Kangkung",
+		"Kubis",
+		"Seledri",
+		"Tauge",
+		"Timun",
+	}
+  
+    // using for loop 
+    for i := 0; i < len(sayuran); i++ { 
+        fmt.Print(i+1,"."," ",sayuran[i], "\n") 
+    } 
+} 
+
+//soal 5
+func Soal5() {
+
+	var satuan = map[string]int{
+		"panjang": 7,
+		"lebar":   4,
+		"tinggi":  6,
+	  }
+
+	  satuan["panjang"] = 7
+	  satuan["lebar"] = 4
+	  satuan["tinggi"] = 6
+	  
+	  var VolumeBalok int
+	  VolumeBalok = satuan["panjang"] * satuan["lebar"] * satuan["tinggi"]
+
+	  fmt.Println("panjang","=",satuan["panjang"])
+	  fmt.Println("lebar","=",satuan["lebar"]) 
+	  fmt.Println("tinggi","=",satuan["tinggi"])
+	  fmt.Println("Volume Balok","=", VolumeBalok)
+
+}
+
+		
+
+func main() {
+
+	fmt.Println("Soal 1")
+	Soal1()
+	fmt.Println()
+
+	fmt.Println("Soal 2")
+	Soal2()
+	fmt.Println()
+
+	fmt.Println("Soal 3")
+	Soal3()
+	fmt.Println()
+
+	fmt.Println("Soal 4")
+	Soal4()
+	fmt.Println()
+
+	fmt.Println("Soal 5")
+	Soal5()
+	fmt.Println()
+
+
+}
+
